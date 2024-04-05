c92189d54489b4a9e11ceafd62e68fce86db0b0e
wangeve000 <164774478+wangeve000@users.noreply.github.com>
2024-04-05 07:53:08 -0500

    Update forms.py

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import PasswordChangeForm
-from django.contrib.auth.forms import UserCreationForm
 
+#allowing users to change their password once logged in
 class userPasswordChangeForm(PasswordChangeForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -9,12 +9,3 @@ class userPasswordChangeForm(PasswordChangeForm):
         self.fields['new_password1'].widget.attrs['class'] = 'form-control'
         self.fields['new_password2'].widget.attrs['class'] = 'form-control'
         self.fields['new_password1'].help_text = None
-
-
-class UserCreateForm(UserCreationForm):
-    def __init__(self, *args, **kwargs):
-        super(UserCreateForm, self).__init__(*args, **kwargs)
-        for fieldname in ['username', 'password1', 'password2']:
-            self.fields[fieldname].help_text = None
-            self.fields[fieldname].widget.attrs.update({'class': 'form-control'})
-
