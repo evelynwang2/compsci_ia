22413a5dfe68581a0fc7814cc888511095b17928
wangeve000 <164774478+wangeve000@users.noreply.github.com>
2024-04-05 07:50:42 -0500

    Update apps.py

--- accounts/apps.py
+++ accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
-
+#adding the ability to create accounts for the admin
 class AccountsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'accounts'
