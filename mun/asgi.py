26cfff5f5af27d54f8913685ca7002dab23dc537
wangeve000 <164774478+wangeve000@users.noreply.github.com>
2024-04-05 08:02:05 -0500

    Update asgi.py

--- mun/asgi.py
+++ mun/asgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-
+#allows for the 'app' to be hosted
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mun.settings')
 
 application = get_asgi_application()
