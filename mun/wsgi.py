fd6b39f32b1bcfe746317434052a459ed89fa9f6
wangeve000 <164774478+wangeve000@users.noreply.github.com>
2024-04-05 08:05:05 -0500

    Update wsgi.py

--- mun/wsgi.py
+++ mun/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+#works with the asgi to ge the website to run
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mun.settings')
 
 application = get_wsgi_application()
